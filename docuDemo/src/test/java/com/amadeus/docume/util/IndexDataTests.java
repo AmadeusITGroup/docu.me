0215430d38077252259f61c21c41a55bd99e2bd5
Neha GHATE <nghate@amadeus.com>
2016-12-05 15:23:43 -0500

    added unit test for indexdata

--- docuDemo/src/test/java/com/amadeus/docume/util/IndexDataTests.java
+++ docuDemo/src/test/java/com/amadeus/docume/util/IndexDataTests.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.amadeus.util;
+package com.amadeus.docume.util;
 
 import static org.junit.Assert.*;
 
@@ -13,9 +13,7 @@ import java.util.Map;
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
-import com.amadeus.docuMe.util.ApiData;
-import com.amadeus.docuMe.util.Documentation;
-import com.amadeus.docuMe.util.IndexData;
+import com.amadeus.docume.util.IndexData;
 
 import io.swagger.models.Model;
 import io.swagger.models.Operation;
@@ -29,7 +27,7 @@ import io.swagger.parser.SwaggerParser;
  */
 public class IndexDataTests {
 
-	String swaggerFile = "d:\\Userfiles\\nghate\\Desktop\\hotelapi.yml";
+	String swaggerFile = "d:\\Userfiles\\nghate\\Desktop\\swg.yml";
 	File file = new File(swaggerFile);
 
 	/**
@@ -37,14 +35,14 @@ public class IndexDataTests {
 	 */
 	@Test
 	public void testCreateIndexData() {
-		Swagger swaggerObj = null;
+		Swagger swagger = null;
 		if (file.exists()) {
-			swaggerObj = new SwaggerParser().read(swaggerFile);
+			swagger = new SwaggerParser().read(swaggerFile);
 		}
 		List<String> operationIdList = new ArrayList<>();
 		List<String> modelsList = new ArrayList<>();
-		Map<String, Path> pathMap = swaggerObj.getPaths();
-		Map<String, Model> modelMap = swaggerObj.getDefinitions();
+		Map<String, Path> pathMap = swagger.getPaths();
+		Map<String, Model> modelMap = swagger.getDefinitions();
 
 		for (Map.Entry<String, Path> pathDetail : pathMap.entrySet()) {
 			Path path = pathDetail.getValue();
@@ -53,6 +51,120 @@ public class IndexDataTests {
 				operationIdList.add(op.getOperationId());
 			}
 		}
+		List<String> expectedOperationIdList = new ArrayList<>();
+		expectedOperationIdList.add("Airport Autocomplete");
+		expectedOperationIdList.add("Nearest Relevant Airport");
+		expectedOperationIdList.add("Car Rental Airport Search");
+		expectedOperationIdList.add("Car Rental Geosearch");
+		expectedOperationIdList.add("Flight Affiliate Search");
+		expectedOperationIdList.add("Flight Extensive Search");
+		expectedOperationIdList.add("Flight Inspiration Search");
+		expectedOperationIdList.add("Flight Low-Fare Search");
+		expectedOperationIdList.add("Hotel Airport Search");
+		expectedOperationIdList.add("Hotel Geosearch by box");
+		expectedOperationIdList.add("Hotel Geosearch by circle");
+		expectedOperationIdList.add("Hotel Property Code Search");
+		expectedOperationIdList.add("Location Information");
+		expectedOperationIdList.add("YapQ Geosearch");
+		expectedOperationIdList.add("YapQ City Name Search");
+		expectedOperationIdList.add("Rail-Station Information");
+		expectedOperationIdList.add("Rail-Station Autocomplete");
+		expectedOperationIdList.add("Train Extensive Search");
+		expectedOperationIdList.add("Train Schedule Search");
+		expectedOperationIdList.add("Flight Traffic API");
+		expectedOperationIdList.add("Top Flight Destinations");
+		expectedOperationIdList.add("Top Flight Searches");
+		expectedOperationIdList.add("Travel Record Retrieve");
+		
+		assertArrayEquals(expectedOperationIdList.toArray(), operationIdList.toArray());
+		
+		List<String> expectedModelList = new ArrayList<>();
+		expectedModelList.add("Error");
+		expectedModelList.add("ExtremeSearchResponse");
+		expectedModelList.add("ExtremeSearchResult");
+		expectedModelList.add("LowFareSearchResponse");
+		expectedModelList.add("LowFareSearchResult");
+		expectedModelList.add("FlightSearchItinerary");
+		expectedModelList.add("FlightSearchBound");
+		expectedModelList.add("FlightSearchSegment");
+		expectedModelList.add("Airport");
+		expectedModelList.add("FlightSearchBookingInfo");
+		expectedModelList.add("FlightSearchPrice");
+		expectedModelList.add("FareRestrictions");
+		expectedModelList.add("Fare");
+		expectedModelList.add("AirportAutocompleteResponse");
+		expectedModelList.add("RailStationAutocompleteResponse");
+		expectedModelList.add("LocationResponse");
+		expectedModelList.add("CityInformation");
+		expectedModelList.add("AirportInformation");
+		expectedModelList.add("NearestAirport");
+		expectedModelList.add("Geolocation");
+		expectedModelList.add("HotelSearchResponse");
+		expectedModelList.add("HotelPropertyResponse");
+		expectedModelList.add("Address");
+		expectedModelList.add("Contact");
+		expectedModelList.add("Amenity");
+		expectedModelList.add("Award");
+		expectedModelList.add("Image");
+		expectedModelList.add("HotelRoom");
+		expectedModelList.add("RoomInfo");
+		expectedModelList.add("Amount");
+		expectedModelList.add("PeriodRate");
+		expectedModelList.add("Link");
+		expectedModelList.add("CarSearchResponse");
+		expectedModelList.add("CarSearchResult");
+		expectedModelList.add("Company");
+		expectedModelList.add("Vehicle");
+		expectedModelList.add("VehicleInfo");
+		expectedModelList.add("Rate");
+		expectedModelList.add("ExtensiveTrainSearchResponse");
+		expectedModelList.add("ExtensiveTrainSearchResult");
+		expectedModelList.add("TrainSearchItinerary");
+		expectedModelList.add("Station");
+		expectedModelList.add("TrainSearchSegment");
+		expectedModelList.add("TrainSearchPricing");
+		expectedModelList.add("RestrictedRate");
+		expectedModelList.add("RailStationResponse");
+		expectedModelList.add("TrainScheduleSearchResponse");
+		expectedModelList.add("TrainScheduleSearchResult");
+		expectedModelList.add("RailService");
+		expectedModelList.add("TravelRecordResponse");
+		expectedModelList.add("TravelRecordHeader");
+		expectedModelList.add("Message");
+		expectedModelList.add("Traveler");
+		expectedModelList.add("Infant");
+		expectedModelList.add("FrequentTravelerCard");
+		expectedModelList.add("Reservation");
+		expectedModelList.add("FlightTicket");
+		expectedModelList.add("FlightReservationBound");
+		expectedModelList.add("FlightReservationSegment");
+		expectedModelList.add("FlightReservationBookingInfo");
+		expectedModelList.add("CarReservation");
+		expectedModelList.add("CarReservationBookingInfo");
+		expectedModelList.add("HotelReservation");
+		expectedModelList.add("HotelReservationBookingInfo");
+		expectedModelList.add("OtherReservation");
+		expectedModelList.add("OtherReservationBookingInfo");
+		expectedModelList.add("AffiliateSearchResponse");
+		expectedModelList.add("AffiliateSearchMeta");
+		expectedModelList.add("CarrierMeta");
+		expectedModelList.add("CarrierInfo");
+		expectedModelList.add("Logos");
+		expectedModelList.add("AffiliateSearchResult");
+		expectedModelList.add("AffiliatePayout");
+		expectedModelList.add("AffiliateFlightSearchPrice");
+		expectedModelList.add("Fees");
+		expectedModelList.add("TopDestinationsSearchResponse");
+		expectedModelList.add("TopDestinationsSearchResult");
+		expectedModelList.add("TopSearchesSearchResponse");
+		expectedModelList.add("TopSearchesSearchResult");
+		expectedModelList.add("FlightTrafficSearchResponse");
+		expectedModelList.add("FlightTrafficSearchResult");
+		expectedModelList.add("PointsOfInterestResponse");
+		expectedModelList.add("PointOfInterestCity");
+		expectedModelList.add("PointOfInterestResult");
+		expectedModelList.add("PointOfInterestDetails");
+		expectedModelList.add("ImageSize");
 		
 		if (modelMap != null) {
 			// Add model's list in index file
@@ -61,18 +173,7 @@ public class IndexDataTests {
 			}
 		}
 		
-		if (file.exists()) {
-			swaggerObj = new SwaggerParser().read(swaggerFile);
-		}
-		IndexData indexDataObj = new IndexData();
-		String indexData = indexDataObj.createIndexData(swaggerObj);
-		for(String operation:operationIdList){
-			assertThat(indexData,CoreMatchers.containsString(operation));
-		}
-		for(String modelName:modelsList){
-			assertThat(indexData,CoreMatchers.containsString(modelName));
-		}
-		
+		assertEquals(expectedModelList, modelsList);
 	}
 
 }
